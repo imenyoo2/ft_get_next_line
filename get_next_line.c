9f96f9f0e44bbea2b99c0ab4066e1cf09c4cce0c
Ayoub Ait El Aouad <ayait-el@e3r8p5.1337.ma>
2023-11-23 20:34:32 +0100

    fixed everything finally

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,23 @@
 /*   By: ayait-el <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 11:05:59 by ayait-el          #+#    #+#             */
-/*   Updated: 2023/11/23 19:55:14 by ayait-el         ###   ########.fr       */
+/*   Updated: 2023/11/23 20:26:40 by ayait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int alloc_new_line(t_state **state, t_line *line, char *buffer)
+static int	alloc_new_line(t_state **state, t_line *line, char *buffer,
+		int size)
 {
-  line->line = ft_realloc(NULL, 0, (*state)->remained, (*state)->remained_size);
-  if (line->line == NULL)
-  {
-    ft_free(state, buffer, line->line, BUFFER | STATE);
-    return (1);
-  }
-  line->size = (*state)->remained_size;
-  return (0);
+	line->line = ft_realloc(NULL, 0, (*state)->remained, size);
+	if (line->line == NULL)
+	{
+		ft_free(state, buffer, line->line, BUFFER | STATE);
+		return (1);
+	}
+	line->size = size;
+	return (0);
 }
 
 static int	check_remained(t_state **state, char *buffer, t_line *line)
@@ -33,33 +34,30 @@ static int	check_remained(t_state **state, char *buffer, t_line *line)
 		tmp = check_new_line((*state)->remained, (*state)->remained_size);
 		if (tmp)
 		{
-			line->line = ft_realloc(NULL, 0, (*state)->remained, tmp
-					- (*state)->remained + 1);
-			if (line->line == NULL)
-			{
-				ft_free(state, buffer, line->line, STATE | BUFFER);
+			if (alloc_new_line(state, line, buffer, tmp - (*state)->remained
+					+ 1))
 				return (1);
-			}
-			ft_memcpy((*state)->remained, tmp + 1, (*state)->remained_size - (tmp
-					- (*state)->remained + 1));
+			ft_memcpy((*state)->remained, tmp + 1, (*state)->remained_size
+				- (tmp - (*state)->remained + 1));
 			(*state)->remained_size -= (tmp - (*state)->remained + 1);
 			ft_free(state, buffer, line->line, BUFFER);
 			return (1);
 		}
-    if (alloc_new_line(state, line, buffer))
-      return (1);
+		if (alloc_new_line(state, line, buffer, (*state)->remained_size))
+			return (1);
 		(*state)->remained_size = 0;
 	}
 	return (0);
 }
 
-static int	realloc_line(t_state **state, t_line *line, int readed, char *buffer)
+static int	realloc_line(t_state **state, t_line *line, int readed,
+		char *buffer)
 {
 	line->line = ft_realloc(line->line, line->size, buffer, readed);
 	if (line->line == NULL)
 	{
 		ft_free(state, buffer, line->line, STATE | BUFFER);
-		return (readed);
+		return (1);
 	}
 	line->size += readed;
 	return (0);
@@ -76,13 +74,8 @@ static int	read_line(t_state **state, char *buffer, t_line *line, int fd)
 		tmp = check_new_line(buffer, readed);
 		if (tmp)
 		{
-			line->line = ft_realloc(line->line, line->size, buffer, tmp - buffer
-					+ 1);
-			if (line->line == NULL)
-			{
-				ft_free(state, buffer, line->line, STATE | BUFFER);
+			if (realloc_line(state, line, tmp - buffer + 1, buffer))
 				return (readed);
-			}
 			ft_memcpy((*state)->remained, tmp + 1, readed - (tmp - buffer + 1));
 			(*state)->remained_size = readed - (tmp - buffer + 1);
 			ft_free(state, buffer, line->line, BUFFER);
@@ -118,26 +111,3 @@ char	*get_next_line(int fd)
 		ft_free(&state, buffer, line.line, LINE);
 	return (NULL);
 }
-
-/*
-#include <fcntl.h>
-#include <stdio.h>
-
-int	main(void)
-{
-	int fd = open("variable_nls.txt", O_RDONLY);
-	printf("expected \n, got %s\n", get_next_line(fd));
-	printf("expected: 0123456789012345678901234567890123456789x2\n, got: %s", get_next_line(fd));
-	printf("expected: 0123456789012345678901234567890123456789x3\n, got %s", get_next_line(fd));
-	printf("expected: \n, got %s\n", get_next_line(fd));
-	printf("expected: 0123456789012345678901234567890123456789x5\n, got: %s", get_next_line(fd));
-	printf("expected \n, got %s\n", get_next_line(fd));
-	printf("expected \n, got %s\n", get_next_line(fd));
-	printf("expected: 0123456789012345678901234567890123456789x8\n, got: %s", get_next_line(fd));
-	printf("expected: \n, got %s\n", get_next_line(fd));
-	printf("expected: \n, got %s\n", get_next_line(fd));
-	printf("expected: \n, got %s\n", get_next_line(fd));
-	printf("expected: 0123456789012345678901234567890123456789x12, got: %s\n", get_next_line(fd));
-	printf("expected: NULL, got %s\n", get_next_line(fd));
-}
-*/
--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,42 +6,64 @@
 /*   By: ayait-el <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 11:05:59 by ayait-el          #+#    #+#             */
-/*   Updated: 2023/11/23 19:56:46 by ayait-el         ###   ########.fr       */
+/*   Updated: 2023/11/23 20:33:29 by ayait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-static int	check_remained(t_state *state, char *buffer, t_line *line)
+static int	alloc_new_line(t_state **state, t_line *line, char *buffer,
+		int size)
+{
+	line->line = ft_realloc(NULL, 0, (*state)->remained, size);
+	if (line->line == NULL)
+	{
+		ft_free(state, buffer, line->line, BUFFER | STATE);
+		return (1);
+	}
+	line->size = size;
+	return (0);
+}
+
+static int	check_remained(t_state **state, char *buffer, t_line *line)
 {
 	char	*tmp;
 
-	if (state->remained_size)
+	if ((*state)->remained_size)
 	{
-		tmp = check_new_line(state->remained, state->remained_size);
+		tmp = check_new_line((*state)->remained, (*state)->remained_size);
 		if (tmp)
 		{
-			line->line = ft_realloc(NULL, 0, state->remained, tmp
-					- state->remained + 1);
-			if (line->line == NULL)
-			{
-				ft_free(&state, buffer, line->line, STATE | BUFFER);
+			if (alloc_new_line(state, line, buffer, tmp - (*state)->remained
+					+ 1))
 				return (1);
-			}
-			ft_memcpy(state->remained, tmp + 1, state->remained_size - (tmp
-					- state->remained + 1));
-			state->remained_size -= (tmp - state->remained + 1);
-			ft_free(&state, buffer, line->line, BUFFER);
+			ft_memcpy((*state)->remained, tmp + 1, (*state)->remained_size
+				- (tmp - (*state)->remained + 1));
+			(*state)->remained_size -= (tmp - (*state)->remained + 1);
+			ft_free(state, buffer, line->line, BUFFER);
 			return (1);
 		}
-		line->line = ft_realloc(NULL, 0, state->remained, state->remained_size);
-		line->size = state->remained_size;
-		state->remained_size = 0;
+		if (alloc_new_line(state, line, buffer, (*state)->remained_size))
+			return (1);
+		(*state)->remained_size = 0;
 	}
 	return (0);
 }
 
-static int	read_line(t_state *state, char *buffer, t_line *line, int fd)
+static int	realloc_line(t_state **state, t_line *line, int readed,
+		char *buffer)
+{
+	line->line = ft_realloc(line->line, line->size, buffer, readed);
+	if (line->line == NULL)
+	{
+		ft_free(state, buffer, line->line, STATE | BUFFER);
+		return (1);
+	}
+	line->size += readed;
+	return (0);
+}
+
+static int	read_line(t_state **state, char *buffer, t_line *line, int fd)
 {
 	char	*tmp;
 	int		readed;
@@ -52,26 +74,16 @@ static int	read_line(t_state *state, char *buffer, t_line *line, int fd)
 		tmp = check_new_line(buffer, readed);
 		if (tmp)
 		{
-			line->line = ft_realloc(line->line, line->size, buffer, tmp - buffer
-					+ 1);
-			if (line->line == NULL)
-			{
-				ft_free(&state, buffer, line->line, STATE | BUFFER);
+			if (realloc_line(state, line, tmp - buffer + 1, buffer))
 				return (readed);
-			}
-			ft_memcpy(state->remained, tmp + 1, readed - (tmp - buffer + 1));
-			state->remained_size = readed - (tmp - buffer + 1);
-			ft_free(&state, buffer, line->line, BUFFER);
+			ft_memcpy((*state)->remained, tmp + 1, readed - (tmp - buffer + 1));
+			(*state)->remained_size = readed - (tmp - buffer + 1);
+			ft_free(state, buffer, line->line, BUFFER);
 			return (readed);
 		}
-		line->line = ft_realloc(line->line, line->size, buffer, readed);
-		if (line->line == NULL)
-		{
-			ft_free(&state, buffer, line->line, STATE | BUFFER);
+		if (realloc_line(state, line, readed, buffer))
 			return (readed);
-		}
-		line->size += readed;
-    readed = read(fd, buffer, BUFFER_SIZE);
+		readed = read(fd, buffer, BUFFER_SIZE);
 	}
 	return (readed);
 }
@@ -83,13 +95,13 @@ char	*get_next_line(int fd)
 	int				readed;
 	t_line			line;
 
-	if (fd < 0 || fd > OPEN_MAX)
+	if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (!init_state(&state_arr[fd], &line, &buffer))
 		return (NULL);
-	if (check_remained(state_arr[fd], buffer, &line))
+	if (check_remained(&state_arr[fd], buffer, &line))
 		return (line.line);
-	readed = read_line(state_arr[fd], buffer, &line, fd);
+	readed = read_line(&state_arr[fd], buffer, &line, fd);
 	if (readed > 0)
 		return (line.line);
 	ft_free(&state_arr[fd], buffer, line.line, STATE | BUFFER);
@@ -98,39 +110,3 @@ char	*get_next_line(int fd)
 	ft_free(&state_arr[fd], buffer, line.line, LINE);
 	return (NULL);
 }
-
-/*
-#include <fcntl.h>
-#include <stdio.h>
-
-int	main(void)
-{
-	char	*name;
-	int		fd;
-	char	*temp;
-
-  //int fd = open("1char.txt", O_RDONLY);
-  //close(fd);
-//  char *line;
-//
-//  while ((line = get_next_line(100)))
-//  {
-//    printf("%s", line);
-//    free(line);
-//  }
-//  printf("%s\n", line);
-  name = "read_error.txt";
-  fd = open(name, O_RDONLY);
-  printf("expeced: aaaaaaaaaa\ngot: %s", get_next_line(fd));
-  printf("expeced: bbbbbbbbbb\ngot: %s", get_next_line(fd));
-  // set the next read call to return -1
-  //next_read_error = 1;
-  if (BUFFER_SIZE >= 100) {
-	do {
-		temp = get_next_line(fd);
-		free(temp);
-	} while (temp != NULL);
-  }
-  printf("expeced null, got %s\n", get_next_line(fd));
-}
-*/
